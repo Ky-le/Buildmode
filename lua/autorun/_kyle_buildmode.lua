60def7602522072728125f492fc1573aaab01159
Kyle Reid <thisisnotkylesemail@gmail.com>
2016-06-16 18:26:45 -0500

    Update _kyle_buildmode.lua

--- lua/autorun/_kyle_buildmode.lua
+++ lua/autorun/_kyle_buildmode.lua
@@ -1,124 +1,93 @@
-AddCSLuaFile()
-CreateConVar("_kyle_builderHighlight", "0", 8192, "Toggle whether Builders are outlined. (0-1)")
-CreateConVar("_kyle_builderHighlightR", "0", 8192, "Change the RED hue of the Builder outline. (0-255)")
-CreateConVar("_kyle_builderHighlightG", "128", 8192, "Change the GREEN hue of the Builder outline. (0-255)")
-CreateConVar("_kyle_builderHighlightB", "255", 8192, "Change the BLUE hue of the Builder outline. (0-255)")
-CreateConVar("_kyle_builderExHighlight", "0", 8192, "Toggle whether Non-Builders are outlined. (0-1)")
-CreateConVar("_kyle_builderExHighlightR", "255", 8192, "Change the RED hue of the Non-Builder outline. (0-255)")
-CreateConVar("_kyle_builderExHighlightG", "0", 8192, "Change the GREEN hue of the Non-Builder outline. (0-255)")
-CreateConVar("_kyle_builderExHighlightB", "128", 8192, "Change the BLUE hue of the Non-Builder outline. (0-255)")
-CreateConVar("_kyle_builderCommand", "!buildmode", 8192, "Change the command to toggle Buildmode for all clients.")
-CreateConVar("_kyle_builderOnSpawn", "0", 8192, "Toggle whether Buildmode is enabled by default. (0-1)")
-CreateConVar("_kyle_builderAbuseKick", "0", 8192, "Toggle whether Builders are kicked for abusing Buildmode. (0-1)")
+local CATEGORY_NAME = "Nigger"
+
+------------------------------ Buildmode ------------------------------
+--Weapons the player can spawn while in Buildmode
  _kyle_builderSpawnableWeapons={
-"weapon_physgun",
-"gmod_tool",
-"gmod_camera"
+ 	"weapon_physgun",
+	"gmod_tool",
+	"gmod_camera"
 }
+--Weapons the player gets reset to when switched to Buildmode
  _kyle_builderWeapons={
-"weapon_physgun",
-"gmod_tool",
-"gmod_camera"
+ 	"weapon_physgun",
+ 	"gmod_tool",
+ 	"gmod_camera"
 }
-function _kyle_buildmodeToggle(ply)
-        if (ply:GetNWInt("_kyle_buildmode") == 1) then  
-            ply:SetNWInt("_kyle_buildmode", 0) 
-            ply:SendLua("GAMEMODE:AddNotify(\"Build Mode disabled.\",NOTIFY_GENERIC, 5)")
-            PrintMessage( HUD_PRINTTALK, ply:GetName( ) .." has disabled Build Mode.")
-        elseif (ply:GetNWInt("_kyle_buildmode") == 0) then
-            _kyle_buildweapons(ply)
-            ply:SetNWInt("_kyle_buildmode", 1) 
-            ply:SendLua("GAMEMODE:AddNotify(\"Build Mode enabled.\",NOTIFY_GENERIC, 5)")
-            PrintMessage( HUD_PRINTTALK, ply:GetName( ) .." has enabled Build Mode.")
-        end
-end
 function _kyle_buildweapons(ply)
-    ply:StripWeapons()
-    for i=1,#_kyle_builderWeapons do 
+	ply:StripWeapons()
+	for i=1,#_kyle_builderWeapons do 
         ply:Give(_kyle_builderWeapons[i])
     end
 end
-hook.Add( "PlayerSpawn", "OnSpawn", function( ply )
-    if GetConVar("_kyle_builderOnSpawn"):GetInt() ==1 then _kyle_builderOnSpawn=true end
-    if GetConVar("_kyle_builderOnSpawn"):GetInt() ==0 then _kyle_builderOnSpawn=false end
-    if GetConVar("_kyle_builderAbuseKick"):GetInt() ==1 then _kyle_builderAbuseKick=true end
-    if GetConVar("_kyle_builderAbuseKick"):GetInt() ==0 then _kyle_builderAbuseKick=false end
-    ply:SetNWInt("_kyle_buildmode", 0) 
-    if _kyle_builderOnSpawn then
-        ply:SendLua("GAMEMODE:AddNotify(\"You will be invincible until you say " .._kyle_builderCommand..".\",NOTIFY_GENERIC, 5)")
-        ply:SetNWInt("_kyle_buildNotify", 1)
-        _kyle_buildweapons(ply)
-        ply:SetNWInt("_kyle_buildmode", 1) 
-        PrintMessage( HUD_PRINTTALK, ply:GetName( ) .." has spawned with Build Mode.")
-    end
-end )
-hook.Add("PlayerDeath", "OnDeath", function(victim, inflictor, killer)
-    if killer:GetNWInt("_kyle_buildmode")==1 and killer != victim and _kyle_builderAbuseKick then
-        PrintMessage( HUD_PRINTTALK, killer:GetName( ) .." has been kicked for killing " .. victim:GetName( ) .. "while in Buildmode." )
-        killer:Kick("You have killed someone while in Buildmode.")
-    end
-end)
-hook.Add("PlayerSay", "OnSay", function(ply, say)
-    _kyle_builderCommand = GetConVar("_kyle_builderCommand"):GetString()
-	local text = say:lower()
-	if (text == _kyle_builderCommand) then
-        _kyle_buildmodeToggle(ply)
-	end
-end)
-hook.Add("EntityTakeDamage", "GodMode",  function(ply, dmginfo)
-    if(ply:GetNWInt("_kyle_buildmode") == 1 and ply:IsPlayer()) then
-        dmginfo:ScaleDamage( 0 )
-    end
-end )
-hook.Add("PlayerCanPickupWeapon", "TryWepPickup",  function(ply, wep)
-    if (ply:GetNWInt("_kyle_buildmode") == 1) then
-        local weapon = string.Explode( "[", tostring(wep))
-        weapon = string.Explode("]", table.GetLastValue(weapon))
-        table.remove(weapon, 2)
-        if(!table.HasValue(_kyle_builderSpawnableWeapons,table.GetLastValue(weapon))) then
-            if(ply:GetNWInt("_kyle_buildNotify") == 1)then
-                ply:SetNWInt("_kyle_buildNotify", 0)
-                ply:SendLua("GAMEMODE:AddNotify(\"You cannot get weapons while in Build Mode.\",NOTIFY_GENERIC, 5)") 
-            end
-            timer.Create( "_kyle_NotifyBuildmode", 1, 1, function()
-                ply:SetNWInt("_kyle_buildNotify", 1)
-            end)
-            return false
-        end
-    end
-end)
-hook.Add("PlayerSpawnSWEP", "TryWepSpawn",  function(ply)
-    if (ply:GetNWInt("_kyle_buildmode") == 1) then
-        ply:SendLua("GAMEMODE:AddNotify(\"You cannot get weapons while in Build Mode.\",NOTIFY_GENERIC, 5)")
+hook.Add("PlayerGiveSWEP", "_kyle_Buildmode_TrySWEPGive", function(v, wep)
+     if v.buildmode and !table.HasValue(_kyle_builderSpawnableWeapons,wep) then
+        v:SendLua("GAMEMODE:AddNotify(\"You cannot give yourself weapons while in Buildmode.\",NOTIFY_GENERIC, 5)")
         return false
     end
 end)
-hook.Add("PreDrawHalos", "AddHalos", function()
-    if GetConVar("_kyle_builderHighlight"):GetInt() ==1 then _kyle_builderHighlight=true end
-    if GetConVar("_kyle_builderHighlight"):GetInt() ==0 then _kyle_builderHighlight=false end
-    if GetConVar("_kyle_builderExHighlight"):GetInt() ==1 then _kyle_builderExHighlight=true end
-    if GetConVar("_kyle_builderExHighlight"):GetInt() ==0 then _kyle_builderExHighlight=false end
-    if _kyle_builderHighlight or _kyle_builderExHighlight then
-        _kyle_Builders={}
-        _kyle_BuildersEx={}
-        for i=1,#player.GetAll() do
-            if(player.GetAll()[i]:GetNWInt("_kyle_buildmode")==1 and player.GetAll()[i]:Alive() and _kyle_builderHighlight) then 
-                table.insert(_kyle_Builders, player.GetAll()[i])
-            elseif(player.GetAll()[i]:GetNWInt("_kyle_buildmode")==0 and player.GetAll()[i]:Alive() and _kyle_builderExHighlight)then
-                table.insert(_kyle_BuildersEx, player.GetAll()[i])
-            end
+hook.Add("PlayerSpawnSWEP", "_kyle_Buildmode_TrySWEPSpawn", function(v, wep)
+    if v.buildmode and !table.HasValue(_kyle_builderSpawnableWeapons,wep) then
+        v:SendLua("GAMEMODE:AddNotify(\"You cannot spawn weapons while in Buildmode.\",NOTIFY_GENERIC, 5)")
+        return false
+    end
+end)
+hook.Add("PlayerCanPickupWeapon", "_kyle_Buildmode_TrySWEPPickup", function(v, wep)
+    local weapon = string.Explode("]", table.GetLastValue(string.Explode( "[", tostring(wep))))
+    table.remove(weapon, 2)
+    if v.buildmode and !table.HasValue(_kyle_builderSpawnableWeapons,table.GetLastValue(weapon)) then
+        if v:GetNWInt("_kyle_buildNotify") == 1 then
+            v:SetNWInt("_kyle_buildNotify", 0)
+            v:SendLua("GAMEMODE:AddNotify(\"You cannot pick up weapons while in Buildmode.\",NOTIFY_GENERIC, 5)") 
+            timer.Simple( 5, function()
+                v:SetNWInt("_kyle_buildNotify", 1)
+            end)
         end
+        return false   
     end
-    if _kyle_builderHighlight then
-        if (GetConVar("_kyle_builderHighlightR"):GetInt() < 256) and (GetConVar("_kyle_builderHighlightR"):GetInt() > -1) then _kyle_builderHighlightR = GetConVar("_kyle_builderHighlightR"):GetInt() end
-        if (GetConVar("_kyle_builderHighlightG"):GetInt() < 256) and (GetConVar("_kyle_builderHighlightG"):GetInt() > -1) then _kyle_builderHighlightG = GetConVar("_kyle_builderHighlightG"):GetInt() end
-        if (GetConVar("_kyle_builderHighlightB"):GetInt() < 256) and (GetConVar("_kyle_builderHighlightB"):GetInt() > -1) then _kyle_builderHighlightB = GetConVar("_kyle_builderHighlightB"):GetInt() end
-        halo.Add(_kyle_Builders, Color(_kyle_builderHighlightR, _kyle_builderHighlightG, _kyle_builderHighlightB), 4, 4, 1, true)
+end)
+hook.Add("DoPlayerDeath", "_kyle_Buildmode_OnDeath", function(ply, attacker, dmg )
+    if attacker.buildmode then
+        --attacker:Kick("You have killed someone while in Buildmode.")
+        ulx.fancyLogAdmin( attacker, "#A killed #T while in Buildmode using "..tostring(dmg:GetInflictor()), ply)
     end
-    if _kyle_builderExHighlight then
-        if (GetConVar("_kyle_builderExHighlightR"):GetInt() < 256) and (GetConVar("_kyle_builderExHighlightR"):GetInt() > -1) then _kyle_builderExHighlightR = GetConVar("_kyle_builderExHighlightR"):GetInt() end
-        if (GetConVar("_kyle_builderExHighlightG"):GetInt() < 256) and (GetConVar("_kyle_builderExHighlightG"):GetInt() > -1) then _kyle_builderExHighlightG = GetConVar("_kyle_builderExHighlightG"):GetInt() end
-        if (GetConVar("_kyle_builderExHighlightB"):GetInt() < 256) and (GetConVar("_kyle_builderExHighlightB"):GetInt() > -1) then _kyle_builderExHighlightB = GetConVar("_kyle_builderExHighlightB"):GetInt() end
-        halo.Add(_kyle_BuildersEx, Color(_kyle_builderExHighlightR, _kyle_builderExHighlightG, _kyle_builderExHighlightB), 4, 4, 1, true)
-    end
-end )
+end)
+function ulx.buildmode( calling_ply, target_plys, should_revoke )
+    local affected_plys = {}
+	for i=1, #target_plys do
+		if not should_revoke and ulx.getExclusive( target_plys[ i ], calling_ply ) then
+			ULib.tsayError( calling_ply, ulx.getExclusive( target_plys[ i ], calling_ply ), true )
+		else
+            local v = target_plys[ i ]
+            if v.buildmode == nil && not should_revoke then
+            	ulx.setExclusive( v, "in Buildmode" )
+                ULib.getSpawnInfo( v )
+                v:StripWeapons()
+                _kyle_buildweapons(v)
+                v:GodEnable()
+                v.ULXHasGod = true
+                v.buildmode = true
+			elseif v.buildmode != nil && should_revoke then
+				ulx.clearExclusive( v )
+                v:GodDisable()
+                v.ULXHasGod = nil
+                v.buildmode = nil
+                local pos = v:GetPos()
+                ULib.spawn( v, true )
+                v:SetPos( pos )
+			end
+            table.insert( affected_plys, v )
+        end
+	end
+
+	if not should_revoke then
+		ulx.fancyLogAdmin( calling_ply, "#A granted Buildmode mode upon #T", affected_plys )
+	else
+		ulx.fancyLogAdmin( calling_ply, "#A revoked Buildmode mode from #T", affected_plys )
+	end
+end
+local buildmode = ulx.command( CATEGORY_NAME, "ulx buildmode", ulx.buildmode, "!buildmode" )
+buildmode:addParam{ type=ULib.cmds.PlayersArg }
+buildmode:defaultAccess( ULib.ACCESS_ADMIN )
+buildmode:addParam{ type=ULib.cmds.BoolArg, invisible=true }
+buildmode:help( "Grants Buildmode mode to target(s)." )
+buildmode:setOpposite( "ulx unbuildmode", {_, _, true}, "!unbuildmode" )
